eba543d99e6a17e4d3e2eb6234e9e4dcd877fb0a
Romain Caplier <caplierr@desktop-042>
2021-02-26 14:06:24 +0100

    Updated .gitignore for data_512crop folders Added img_crop.py scripts

--- datasets/Corona/downloader.py
+++ datasets/Corona/downloader.py
@@ -14,25 +14,23 @@ df_Corona['Url'] = df_Corona_full.iloc[::2].reset_index(drop=True)
 
 # ------------------------------------------------------------------------------
 
-errors = []
-
-for img_data in tqdm(df_Corona.values):
-
-    output_path = 'data/' + img_data[0]
-    url = img_data[1]
-
-    try:
-        r = requests.get(url, stream=True)
-
-        if r.status_code == 200:
-            r.raw.decode_content = True
-
-            with open(output_path,'wb') as f:
-                shutil.copyfileobj(r.raw, f)
-
-    except:
-        errors.append(img_data)
-
 with open('errors.txt', 'w') as f:
-    for error in errors:
-        f.write(' '.join(error) + '\n')
+    for img_data in tqdm(df_Corona.values[400:]):
+
+        output_path = 'data/' + img_data[0]
+        url = img_data[1]
+
+        try:
+            r = requests.get(url, stream=True)
+
+            if r.status_code // 100 == 2:
+                r.raw.decode_content = True
+
+                with open(output_path,'wb') as f_img:
+                    shutil.copyfileobj(r.raw, f_img)
+
+            else:
+                f.write(' '.join(img_data) + ' else ' + str(r.status_code) + '\n')
+
+        except:
+            f.write(' '.join(img_data) + ' except ' + str(r.status_code) + '\n')
