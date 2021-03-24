28ab166eca15c78bae890b8cdd0f4218174dc405
Romain Caplier <romain.caplier@student-cs.fr>
2021-03-24 20:37:06 +0100

    Modified test_gricad

--- scripts/test_gricad.sh
+++ scripts/test_gricad.sh
@@ -4,5 +4,8 @@
 #OAR --stderr test_gricad.err
 #OAR --project cg4n6
 
+source applis/environements/conda.sh
+conda activate CGDetection
+
 cd ~/code/CGvsNI/src
 python main.py --train-test --data CIFAR10 --dataset_name CIFAR10-full-10 --img_mode RGB --method TemporalEnsembling --epochs 150
