02ae34ea154a2108b1083841c35096595e698c12
Romain Caplier <romain.caplier@student-cs.fr>
2021-03-24 20:15:36 +0100

    Modified test_gricad script

--- scripts/test_gricad.sh
+++ scripts/test_gricad.sh
@@ -1,2 +1,7 @@
-cd ../src
+#OAR -t gpu -l /nodes=1/gpunodes=1,walltime=00:01:00
+#OAR --stdout test_gricad.out
+#OAR --stderr test_gricad.err
+#OAR --project cg4n6
+
+cd code/CGvsNI/src
 python main.py --train-test --data CIFAR10 --dataset_name CIFAR10-full-10 --img_mode RGB --method TemporalEnsembling --epochs 150
