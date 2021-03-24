11fa2cd99fe437b78d9caab52e3dc6a4ec627047
Romain Caplier <romain.caplier@student-cs.fr>
2021-03-24 20:22:34 +0100

    Removed typo in test_gricad.sh

--- scripts/test_gricad.sh
+++ scripts/test_gricad.sh
@@ -1,7 +1,8 @@
-#OAR -t gpu -l /nodes=1/gpunodes=1,walltime=00:01:00
+#OAR -n Test_Gricad
+#OAR -t gpu -l /nodes=1/gpudevice=1,walltime=00:01:00
 #OAR --stdout test_gricad.out
 #OAR --stderr test_gricad.err
 #OAR --project cg4n6
 
-cd code/CGvsNI/src
+cd ~/code/CGvsNI/src
 python main.py --train-test --data CIFAR10 --dataset_name CIFAR10-full-10 --img_mode RGB --method TemporalEnsembling --epochs 150
