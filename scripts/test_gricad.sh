c01b95caf1cb10cf1541f458a62a86d4d4449c3e
Romain Caplier <romain.caplier@student-cs.fr>
2021-03-24 20:48:36 +0100

    Final modif test_gricad

--- scripts/test_gricad.sh
+++ scripts/test_gricad.sh
@@ -10,5 +10,5 @@
 source applis/environements/conda.sh
 conda activate CGDetection
 
-cd ~/code/CGvsNI/src
+cd ~/code/CGvsNI-SSL/src
 python main.py --train-test --data CIFAR10 --dataset_name CIFAR10-full-10 --img_mode RGB --method TemporalEnsembling --epochs 150
