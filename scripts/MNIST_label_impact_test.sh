500150867a4b104d9a89201391ae35ddc4c52b04
Romain <romain.caplier@student-cs.fr>
2021-03-25 10:59:36 +0100

    Removed old scripts

--- scripts/MNIST_label_impact_test.sh
+++
@@ -1,6 +0,0 @@
-cd ..
-
-cd src
-python main.py --data MNIST --dataset_name MNIST_10 --img_mode L --method TemporalEnsembling --epochs 30 --no-train
-python main.py --data MNIST --dataset_name MNIST_1 --img_mode L --method TemporalEnsembling --epochs 30 --no-train
-python main.py --data MNIST --dataset_name MNIST_0.1 --img_mode L --method TemporalEnsembling --epochs 30 --no-train
