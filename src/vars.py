6e228455c237fcc0b7ae2221eb036e8758d0515d
Romain Caplier Romain.Caplier@grenoble-inp.fr <caplierr@f-dahu.u-ga.fr>
2021-03-29 16:48:34 +0200

    Gricad 120 epochs

--- src/vars.py
+++ src/vars.py
@@ -42,7 +42,7 @@ TEST_TRANSFORMS = {
 HYPERPARAMETERS_DEFAULT = {
     'TemporalEnsembling': {
         'alpha': 0.6,
-        'ramp_epochs': 10,
+        'ramp_epochs': 80,
         'ramp_mult': 5,
         'unsup_loss_max_weight': 30.
     }
