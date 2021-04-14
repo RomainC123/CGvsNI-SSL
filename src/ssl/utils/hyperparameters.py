7cfb3576a837f754feaf76f8f4794e34ff3a028b
Romain Caplier <romain.caplier@student-cs.fr>
2021-04-14 16:58:56 +0200

    Typo

--- src/ssl/utils/hyperparameters.py
+++ src/ssl/utils/hyperparameters.py
@@ -1,7 +1,7 @@
 METHODS_DEFAULT = {
     'TemporalEnsembling': {
         'alpha': 0.6,
-        'unsup_loss_ramp_up_epochs': 10,
+        'unsup_loss_ramp_up_epochs': 80,
         'unsup_loss_ramp_up_mult': 5,
         'unsup_loss_max_weight': 30.
     }
