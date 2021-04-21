71b654166c063ab6c06375daeac0ede4a9a02f46
Romain Caplier <romain.caplier@student-cs.fr>
2021-04-21 11:25:06 +0200

    Added SimpleNet model

--- src/ssl/utils/functionalities.py
+++ src/ssl/utils/functionalities.py
@@ -2,6 +2,7 @@ from ..data.image import ImageDatasetContainer
 from ..models.VGG import VGGContainer
 from ..models.CNN import CNNContainer
 from ..models.Resnet import Resnet18Container
+from ..models.SimpleNet import SimpleNetContainer
 from ..optimizers.adam import AdamContainer
 from ..methods.tempens import TemporalEnsembling
 
@@ -13,7 +14,8 @@ DATASETS = {
 MODELS = {
     'VGG': VGGContainer,
     'CNN': CNNContainer,
-    'Resnet18': Resnet18Container
+    'Resnet18': Resnet18Container,
+    'SimpleNet': SimpleNetContainer
 }
 
 OPTIMIZERS = {
