c607e0ddd57944184eeb313227c8b97a76376748
Romain Caplier <romain.caplier@student-cs.fr>
2021-05-03 17:28:46 +0200

    removed verbosity from CGvsNo scripts

--- scripts/CGvsNI/CGvsNI-test-new.sh
+++ scripts/CGvsNI/CGvsNI-test-new.sh
@@ -11,4 +11,4 @@ source /applis/environments/conda.sh
 conda activate CGDetection
 
 cd ~/code/CGvsNI-SSL/src
-python ./main.py --train --data CGvsNI --datasets_to_use Artlantis --label_mode Biclass --nb_samples_total 2048 --nb_samples_test 512 --nb_samples_labeled 256 --img_mode RGB --model ENet --method TemporalEnsemblingNewLoss --epochs 300
+python ./main.py --train --data CGvsNI --datasets_to_use Artlantis --label_mode Biclass --nb_samples_total 2048 --nb_samples_test 512 --nb_samples_labeled 256 --img_mode RGB --model ENet --method TemporalEnsemblingNewLoss --epochs 300 --no-verbose
