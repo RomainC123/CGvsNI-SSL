4b4931aa502b7e0b8f1908c4d71a19396a1d5c2d
Romain Caplier Romain.Caplier@grenoble-inp.fr <caplierr@f-dahu.u-ga.fr>
2021-05-03 10:08:51 +0200

    Curves gricad rapidos

--- scripts/CIFAR10-New-VGG.sh
+++ scripts/CIFAR10-New-VGG.sh
@@ -2,7 +2,7 @@
 
 #OAR -n CIFAR10-New-VGG
 #OAR -t gpu
-#OAR -l /nodes=1/gpudevice=1,walltime=2:00:00
+#OAR -l /nodes=1/gpudevice=1,walltime=4:00:00
 #OAR --stdout scripts_logs/CIFAR10-New-VGG.out
 #OAR --stderr scripts_logs/CIFAR10-New-VGG.err
 #OAR --project pr-cg4n6
