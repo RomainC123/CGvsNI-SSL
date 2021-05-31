5127d156ddd31167a22418f0bc6cdd5fef278ec9
Romain Caplier <romain.caplier@student-cs.fr>
2021-05-31 11:16:05 +0200

    Removed unnecessary indent in scripts maker

--- scripts/scripts_launch.py
+++ scripts/scripts_launch.py
@@ -3,19 +3,19 @@ import os
 def make_script(folder, name, walltime, nb_samples_labeled, max_lr):
 
     cmd = f"#!/bin/bash\n\
-    \n\
-    #OAR -n {name}\n\
-    #OAR -t gpu\n\
-    #OAR -l /nodes=1/gpudevice=1,walltime={walltime}\n\
-    #OAR --stdout scripts_logs/{name}.out\n\
-    #OAR --stderr scripts_logs/{name}.err\n\
-    #OAR --project cg4n6\n\
-    \n\
-    source /applis/environments/conda.sh\n\
-    conda activate CGDetection\n\
-    \n\
-    cd ~/code/CGvsNI-SSL/src\n\
-    python ./main.py --train-test --name {name} --data CIFAR10 --nb_samples_test 10000 --nb_samples_labeled {nb_samples_labeled} --img_mode RGB --model SimpleNet --max_lr {max_lr} --method TemporalEnsemblingNewLoss --epochs 300 --no-verbose"
+\n\
+#OAR -n {name}\n\
+#OAR -t gpu\n\
+#OAR -l /nodes=1/gpudevice=1,walltime={walltime}\n\
+#OAR --stdout scripts_logs/{name}.out\n\
+#OAR --stderr scripts_logs/{name}.err\n\
+#OAR --project cg4n6\n\
+\n\
+source /applis/environments/conda.sh\n\
+conda activate CGDetection\n\
+\n\
+cd ~/code/CGvsNI-SSL/src\n\
+python ./main.py --train-test --name {name} --data CIFAR10 --nb_samples_test 10000 --nb_samples_labeled {nb_samples_labeled} --img_mode RGB --model SimpleNet --max_lr {max_lr} --method TemporalEnsemblingNewLoss --epochs 300 --no-verbose"
 
     with open(os.path.join(folder, f'{name}.sh'), 'w+') as f:
         f.write(cmd)
