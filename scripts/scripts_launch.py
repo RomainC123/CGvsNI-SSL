0a02b7492788fad1592c5f3bf32a1d85b5031fd1
Romain Caplier <romain.caplier@student-cs.fr>
2021-05-31 11:31:56 +0200

    Updated folder structure

--- scripts/scripts_launch.py
+++ scripts/scripts_launch.py
@@ -4,18 +4,18 @@ def make_script(folder, name, walltime, nb_samples_labeled, max_lr):
 
     cmd = f"#!/bin/bash\n\
 \n\
-#OAR -n {name}\n\
+#OAR -n {folder + '_' + name}\n\
 #OAR -t gpu\n\
 #OAR -l /nodes=1/gpudevice=1,walltime={walltime}\n\
-#OAR --stdout {folder}/scripts_logs/{name}.out\n\
-#OAR --stderr {folder}/scripts_logs/{name}.err\n\
+#OAR --stdout {folder}/scripts_logs/{folder + '_' + name}.out\n\
+#OAR --stderr {folder}/scripts_logs/{folder + '_' + name}.err\n\
 #OAR --project cg4n6\n\
 \n\
 source /applis/environments/conda.sh\n\
 conda activate CGDetection\n\
 \n\
 cd ~/code/CGvsNI-SSL/src\n\
-python ./main.py --train-test --name {name} --data CIFAR10 --nb_samples_test 10000 --nb_samples_labeled {nb_samples_labeled} --img_mode RGB --model SimpleNet --max_lr {max_lr} --method TemporalEnsemblingNewLoss --epochs 300 --no-verbose"
+python ./main.py --train-test --folder {folder} --name {name} --data CIFAR10 --nb_samples_test 10000 --nb_samples_labeled {nb_samples_labeled} --img_mode RGB --model SimpleNet --max_lr {max_lr} --method TemporalEnsemblingNewLoss --epochs 300 --no-verbose"
 
     with open(os.path.join(folder, f'{name}.sh'), 'w+') as f:
         f.write(cmd)
@@ -33,7 +33,7 @@ if not os.path.exists(os.path.join(folder, 'scripts_logs')):
     os.makedirs(os.path.join(folder, 'scripts_logs'))
 
 for lr in lr_to_test:
-    make_script(folder, folder + '_' + str(lr), walltime, nb_samples_labeled, lr)
+    make_script(folder, str(lr), walltime, nb_samples_labeled, lr)
 
 for script in os.listdir(folder):
     os.system(f'chmod +x {os.path.join(folder, script)}')
