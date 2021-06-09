65b3657d3b87390816f036c742d6939c45310b34
Romain Caplier Romain.Caplier@grenoble-inp.fr <caplierr@f-dahu.u-ga.fr>
2021-06-09 13:00:24 +0200

    Masse runs gricad

---
+++ scripts/CIFAR10_test_lr/0.01.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#OAR -n CIFAR10_test_lr_0.01
+#OAR -t gpu
+#OAR -l /nodes=1/gpudevice=1,walltime=18:00:00
+#OAR --stdout CIFAR10_test_lr/scripts_logs/CIFAR10_test_lr_0.01.out
+#OAR --stderr CIFAR10_test_lr/scripts_logs/CIFAR10_test_lr_0.01.err
+#OAR --project cg4n6
+
+source /applis/environments/conda.sh
+conda activate CGDetection
+
+cd ~/code/CGvsNI-SSL/src
+python ./main.py --train-test --folder CIFAR10_test_lr --name 0.01 --data CIFAR10 --nb_samples_test 10000 --nb_samples_labeled 1000 --img_mode RGB --model SimpleNet --max_lr 0.01 --method OnlySup --epochs 300 --no-verbose
