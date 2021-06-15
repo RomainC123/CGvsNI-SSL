190545f07860794ad2eb310b29227b3669adb1b8
Romain Caplier <romain.caplier@student-cs.fr>
2021-06-15 18:48:04 +0200

    Updated CGvsNi test script

---
+++ scripts/CGvsNI/CGvsNI-Artlantis-tempens.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#OAR -n CGvsNI-Artlantis-tempens
+#OAR -t gpu
+#OAR -l /nodes=1/gpudevice=1,walltime=12:00:00
+#OAR --stdout scripts_logs/CGvsNI-Artlantis-tempens.out
+#OAR --stderr scripts_logs/CGvsNI-Artlantis-tempens.err
+#OAR --project cg4n6
+
+source /applis/environments/conda.sh
+conda activate CGDetection
+
+cd ~/code/CGvsNI-SSL/src
+python ./main.py --train --data CGvsNI --datasets_to_use Artlantis --label_mode Biclass --nb_samples_total 10800 --nb_samples_test 720 --nb_samples_labeled 1080 --img_mode RGB --model ENet --method TemporalEnsembling --epochs 300 --no-verbose
